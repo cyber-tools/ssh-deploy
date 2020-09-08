b468bfa256584826b65018a29d8961a5ed91e4ea
shicheng.yu <yushicheng@hztianque.com>
2020-09-08 21:08:17 +0800

    2.1.12版本发布

--- scripts/remove-directory.js
+++ scripts/remove-directory.js
@@ -4,7 +4,7 @@ const toast = require("@/utils/toast");
 module.exports = async (client, remote) => {
   try {
     toast.start(["正在移除旧的", remote, "文件夹"].join(""));
-    const rm_result = await client.exec("rm"["-rf", remote]);
+    const rm_result = await client.exec("rm", ["-rf", remote]);
     if (rm_result.stdout) {
       toast.succeed(["旧文件夹", remote, "移除成功!"].join(""));
     };
